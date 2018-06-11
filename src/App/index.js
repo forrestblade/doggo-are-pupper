05d32dfcc103f939c22cd653e6a2bc6a808c31d4
Forrest Carlton <fcarlton@qca-sdc.com>
2018-06-11 12:51:04 -0500

    feat: develop parent index

---
+++ src/App/index.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import PuppersSelection from './PuppersSelection/PuppersSelection'
+import PuppersGallery from './PuppersGallery/PuppersGallery'
+import PropTypes from 'prop-types'
+import {fetchPuppers} from '../Services/pupper'
+
+function handleSelection (choice) {
+  this.props.fetchPuppers(choice)
+  .then(data => {
+    const pupperUrls = data.message.map(url => url)
+    this.setState({
+      puppers: pupperUrls,
+      formError: undefined
+    })
+  })
+  .catch(err => {
+    this.setState({
+      formError: err.error
+    })
+  })
+}
+
+class App extends Component {
+
+  state = {
+    puppers: [],
+    formError: undefined
+  }
+
+  render() {
+    return (
+      <main className='App'>
+      <PuppersSelection handleSubmit={handleSelection.bind(this)} />
+      {this.state.formError && <span className='error'>{this.state.formError}</span>}
+      <PuppersGallery puppers={this.state.puppers} />
+      </main>
+    )
+  }
+}
+
+App.defaultProps = {
+  fetchPuppers
+}
+
+App.propTypes = {
+  fetchPuppers: PropTypes.func
+}
+
+export default App;
